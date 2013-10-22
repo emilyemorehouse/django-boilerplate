6af6ae76f7dcf36d8dfe29dfe42c3c77e3a8677d
Emily Morehouse <emilyemorehouse@gmail.com>
2013-10-22 15:36:55 -0400

    html5 template now works

--- django_boilerplate/django_boilerplate/static/js/init.js
+++ django_boilerplate/django_boilerplate/static/js/init.js
@@ -36,7 +36,7 @@
 
 		// skelJS (probably don't need to change anything here unless you know what you're doing)
 			skelJS: {
-				prefix: 'css/style',
+				prefix: '/static/css/style',
 				resetCSS: true,
 				boxModel: 'border',
 				grid: {
